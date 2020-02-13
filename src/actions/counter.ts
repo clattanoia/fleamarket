eb4f3e0e3db23957b2eb4fd13361df1191461b36
qiuyunfen <yfqiu@thoughtworks.com>
2020-02-13 16:22:34 +0800

    add graphql

--- src/actions/counter.ts
+++ src/actions/counter.ts
@@ -1,3 +1,6 @@
+import { gql } from 'apollo-boost'
+import client from '../graphql-client'
+// import { useQuery, useMutation } from '@apollo/react-hooks'
 import {
   ADD,
   MINUS
@@ -20,5 +23,43 @@ export function asyncAdd () {
     setTimeout(() => {
       dispatch(add())
     }, 2000)
+  }
+}
+
+export function asyncGet() {
+  return dispatch => {
+    const GET_DOGS = gql`
+    {
+      dogs {
+        id
+        breed
+      }
+    }
+  `
+    // const { loading, error } = useQuery(GET_DOGS)
+    //
+    // if (loading) return 'Loading...'
+    // if (error) return `Error! ${error.message}`
+
+    client.query({ query: GET_DOGS })
+
+    dispatch(add())
+  }
+}
+
+export function asyncGraphqlAdd () {
+  return dispatch => {
+    const ADD_TODO = gql`
+      mutation AddTodo($type: String!) {
+        addTodo(type: $type) {
+          id
+          type
+        }
+      }
+    `
+    // useMutation(ADD_TODO)
+    client.mutate({ mutation: ADD_TODO })
+
+    dispatch(add())
   }
 }
---
+++ src/graphql-client.js
@@ -0,0 +1,7 @@
+import ApolloClient from 'apollo-boost'
+
+const client = new ApolloClient({
+  // todo
+  uri: 'https://openapi.baichanghui.com/graphql',
+})
+export default client

56f3baa6b182a646816e2cd64fb0e0151125b8d7
clattanoia <yacnlee@outlook.com>
2020-02-18 15:02:46 +0800

    add graphql client fetch

--- src/graphql-client.js
+++ src/graphql-client.js
@@ -1,7 +1,28 @@
+import Taro from '@tarojs/taro'
 import ApolloClient from 'apollo-boost'
 
 const client = new ApolloClient({
-  // todo
-  uri: 'https://openapi.baichanghui.com/graphql',
+  uri: 'https://miniprogram.yacnlee.top/graphql',
+  fetch: async (url, options) => {
+    try {
+      const { data, statusCode } = await Taro.request({
+        url,
+        method: options.method,
+        data: options.body,
+        header: options.headers
+      })
+      return {
+        ok: () => {
+          return statusCode >= 200 && statusCode < 300
+        },
+        text: () => {
+          return Promise.resolve(JSON.stringify(data))
+        }
+      }
+    } catch (error) {
+      throw error
+    }
+  }
 })
+
 export default client

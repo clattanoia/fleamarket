622a895c62c6a21a32f46d3e2a5c091cf6a87f52
qiuyunfen <yfqiu@thoughtworks.com>
2020-02-27 16:34:04 +0800

    [yunfen] #22 modify category class

--- src/pages/publish/category/index.tsx
+++ src/pages/publish/category/index.tsx
@@ -1,8 +1,8 @@
 import { ComponentClass } from 'react'
 import Taro, { Component } from '@tarojs/taro'
-import { AtIcon } from 'taro-ui'
+import { AtButton, AtFloatLayout, AtIcon, AtRadio } from 'taro-ui'
 import { connect } from '@tarojs/redux'
-import {View, Picker, Text} from '@tarojs/components'
+import { View, Text } from '@tarojs/components'
 
 import './index.scss'
 import FormLine from '../../../components/formLine'
@@ -43,13 +43,18 @@ class Category extends Component {
   state = {
     selector: [],
     selected: '',
+    selectedCategoryName: '',
+    isOpen: false,
   }
 
   componentWillMount () {}
 
   componentDidMount () {
     this.setState({
-      selector: this.props.category.categories.map(item => item.name)
+      selector: this.props.category.categories.map(item => Object.assign({}, {
+        label: item.name,
+        value: item.id
+      }))
     })
   }
 
@@ -59,26 +64,57 @@ class Category extends Component {
 
   componentDidHide () { }
 
-  onCategoryChange = (e) => {
-    const value = this.state.selector[e.detail.value]
-    const item = this.props.category.categories.find(item => item.name === value)
+  onCategoryChange = (val) => {
     this.setState({
-      selected: value
+      selected: val,
     })
-    this.props.onSetVal('selectedCategory', item && item.id)
+  }
+
+  handleValueClick(): void {
+    this.setState({ isOpen: true })
+  }
+
+  onClose = (): void => {
+    this.setState({
+      isOpen: false,
+      selected: this.props.selectedCategory
+    })
+  }
+
+  onConfirm = (): void => {
+    const item = this.props.category.categories.find(item => item.id === this.state.selected)
+    this.setState({
+      isOpen: false,
+      selectedCategoryName: item && item.name
+    })
+    this.props.onSetVal('selectedCategory', this.state.selected)
   }
 
   render () {
     return (
       <View className='category'>
         <FormLine title="分类">
-          <Picker className='picker' mode='selector' range={this.state.selector} onChange={this.onCategoryChange} value={0}>
-            <View className='right-container'>
-              <Text className='category'>{this.state.selected ? this.state.selected : '选择分类'}</Text>
-              <AtIcon prefixClass='iconfont' value='iconright' size="22" color='#999898'></AtIcon>
-            </View>
-          </Picker>
+          <View className='right-container' onClick={this.handleValueClick}>
+            <Text className='category'>{this.state.selectedCategoryName ? this.state.selectedCategoryName : '选择分类'}</Text>
+            <AtIcon prefixClass='iconfont' value='iconright' size="22" color='#999898'></AtIcon>
+          </View>
         </FormLine>
+        <AtFloatLayout isOpened={this.state.isOpen} onClose={this.onClose}>
+          <View className="radio-layout-header">
+            <Text>请选择分类</Text>
+            <AtButton
+              className="category-radio-confirm"
+              type="secondary"
+              size="small"
+              onClick={this.onConfirm}
+            >确定</AtButton>
+          </View>
+          <AtRadio
+            value={this.state.selected}
+            options={this.state.selector}
+            onClick={this.onCategoryChange}
+          />
+        </AtFloatLayout>
       </View>
     )
   }

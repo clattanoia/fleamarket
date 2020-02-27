be6dd10f3406a480e7c2aafa1798bfb0f59136d5
Heyff12 <645068564@qq.com>
2020-02-27 15:44:23 +0800

    add detail /n

--- src/utils/textareaValue.ts
+++ src/utils/textareaValue.ts
@@ -1,11 +1,11 @@
 export function getLineText(publishValue){
-  const valueText = publishValue.replace(/<br\/>/g, '\n')
+  const valueText = publishValue.replace(/<br\/>/g, '\n').replace(/&nbsp;/g, ' ')
   return valueText
 }
 
 
 export function setLineCode(value) {
-  const publishValue = value.replace(/\r{0,}\n/g, '<br/>')
+  const publishValue = value.replace(/\r{0,}\n/g, '<br/>').replace(/\s/g, '&nbsp;')
   return publishValue
 }
 

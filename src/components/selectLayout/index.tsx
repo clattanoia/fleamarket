cc94c06c1640c74aabdd055508d189a4bdd1b9d1
Heyff12 <645068564@qq.com>
2020-03-04 17:18:36 +0800

    fix name

---
+++ src/components/selectLayout/index.tsx
@@ -0,0 +1,61 @@
+import Taro, { memo, useState } from '@tarojs/taro'
+import { View } from '@tarojs/components'
+import { AtIcon }  from 'taro-ui'
+import classNames from 'classnames'
+
+import styles from './index.module.scss'
+
+interface InProps {
+  list: Search.SelectLayout[]
+  current: Search.SelectLayout
+  onChangeSelect: (id) => void
+}
+
+function SelectLayout(props: InProps) {
+  const { list, current, onChangeSelect } = props
+  const [arrotB, setArrowB] = useState(true)
+
+  if(!list || !list.length){
+    return null
+  }
+
+
+  const selectClick = () => {
+    setArrowB(!arrotB)
+  }
+
+  const listClick = (val) => () => {
+    onChangeSelect(val)
+    setArrowB(!false)
+  }
+
+  return (
+    <View className={styles.selectLayout}>
+      <View className={styles.selectLayoutShow} onClick={selectClick}>
+        <View className={styles.selectTitle}>{current.name}</View>
+        <View className={styles.selectIcon}>
+          <AtIcon prefixClass='iconfont' value={arrotB ? 'iconarrowbottom' : 'iconarrowtop'} size="16"></AtIcon>
+        </View>
+      </View>
+      <View
+        className={classNames(styles.selectLayoutList)}
+        style={{ display: arrotB ? 'none' : 'block' }}
+      >
+        {
+          list.map(item =>
+            <View
+              className={classNames(styles.list, { 'selectActive': item.id === current.id  })}
+              onClick={listClick(item)}
+            >
+              {item.name}
+            </View>
+          )
+        }
+      </View>
+    </View>
+  )
+
+}
+
+
+export default memo(SelectLayout)

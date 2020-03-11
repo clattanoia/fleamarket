ac5850dba7d835e4740d72fc2b9f0218409fc81f
Heyff12 <645068564@qq.com>
2020-03-11 13:14:10 +0800

    fix title 2 line

--- src/components/productItem/index.tsx
+++ src/components/productItem/index.tsx
@@ -3,6 +3,7 @@ import { View, Text, Image } from '@tarojs/components'
 import { AtAvatar } from 'taro-ui'
 import styles from './index.module.scss'
 import defaultProductCover from '../../assets/default_product_cover.png'
+import ExtendedContainer from '../extendedContainer'
 
 interface InProps {
   productData: Global.Goods
@@ -16,7 +17,9 @@ function ProductItem(props: InProps) {
       <View className={styles.goodsImage}>
         <Image className={styles.goodsImg} mode="widthFix" src={productData.coverUrl || defaultProductCover} />
       </View>
-      <Text className={styles.goodsName}>{productData.title}</Text>
+      <View className={styles.goodsName}>
+        <ExtendedContainer content={productData.title} maxLine={2} showSwitch={false} />
+      </View>
       <View className={styles.detail}>
         <Text className={styles.goodsPrice}><Text className='unit'>￥</Text>{productData.price}</Text>
         <Text className={styles.goodsTag}>{productData.categoryName}</Text>

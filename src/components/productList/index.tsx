f0a42ef4ba9c94968ca36f9bdb12d702ea5ad67d
Heyff12 <645068564@qq.com>
2020-03-11 14:24:08 +0800

    fix title 2 line

--- src/components/productList/index.tsx
+++ src/components/productList/index.tsx
@@ -2,12 +2,13 @@ import Taro, { memo } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
 import ProductItem from '../productItem'
 import { ProductType } from '../../constants/enums'
-
+// import ProductListPreload from '../preloadProduct'
 import styles from './index.module.scss'
 
 interface InProps {
   productListData: Global.Goods[]
   productType: ProductType
+  showPreload?: boolean
 }
 
 function ProductList(props: InProps) {
@@ -19,6 +20,31 @@ function ProductList(props: InProps) {
       url: `/pages/detail/index?id=${id}&productType=${productType}`,
     })
   }
+
+  // const renderResult = () => {
+  //   return (
+  //     <View className={styles.wrapperList}>
+  //       {
+  //         productListData.length ?
+  //           productListData.map(item =>
+  //             <View className={styles.listItem} key={item.id} onClick={onClickEvent(item.id)}>
+  //               <ProductItem productData={item} />
+  //             </View>
+  //           )
+  //           : (
+  //             <View className={styles.noProduct}><Text>暂无相关数据</Text></View>
+  //           )
+  //       }
+
+  //     </View>
+  //   )
+  // }
+
+  // return (
+  //   <View>{showPreload ? <ProductListPreload/> : renderResult()}</View>
+  // )
+
+
 
   return (
     <View className={styles.wrapperList}>

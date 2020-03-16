4c4743a8b897f87ff0b7d07839819ae7298185d2
ListenSong1013 <uestclin1013@gmail.com>
2020-03-16 14:24:00 +0800

    add certificate to Avatar component

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -3,28 +3,40 @@ import { View, Image } from '@tarojs/components'
 import { ITouchEvent } from '@tarojs/components/types/common'
 
 import './index.scss'
+import certificateIcon from '../../assets/certificate-1.png'
 
 interface InProps {
   userId: string;
   avatarUrl: string;
   avatarSize?: number;
+  certificate?: boolean,
   style?: {
     [key: string]: string
   };
   onClick?: (event: ITouchEvent) => any;
 }
 
-function Avatar(props: InProps) {
-  const defaultSize = 100
+function Avatar({
+  userId = '',
+  avatarUrl = '',
+  avatarSize = 100,
+  onClick = () => {},
+  certificate = false,
+}: InProps) {
   return (
-    <View className="avatar" onClick={props.onClick ? props.onClick : () => {}} data-id={props.userId}>
+    <View className="avatar" onClick={onClick} data-id={userId}>
       <Image
         style={{
-          width: `${props.avatarSize || defaultSize}rpx`,
-          height: `${props.avatarSize || defaultSize}rpx`,
+          width: `${avatarSize}rpx`,
+          height: `${avatarSize}rpx`,
         }}
-        className="image" src={props.avatarUrl}
+        className="image" src={avatarUrl}
       />
+      {
+        certificate ?
+          <Image className="certificate" src={certificateIcon} />
+          : null
+      }
     </View>
   )
 }

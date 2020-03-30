3a58fa83c87b2cdeb0974d6a48c47581df710d1b
Heyff12 <645068564@qq.com>
2020-03-30 13:39:50 +0800

    change count query

--- src/query/count.ts
+++ src/query/count.ts
@@ -1,7 +1,7 @@
 import { gql } from 'apollo-boost'
 
 export const getForSaleProductCountQuery = gql`
-query getForSaleProductCountQuery() {
+query getForSaleProductCountQuery {
   getForSaleProductCount {
     result
   }

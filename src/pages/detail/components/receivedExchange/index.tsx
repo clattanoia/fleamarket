249db296f1961c00328178d6db2873e37bd11586
Enok <416828041@qq.com>
2020-06-04 17:10:05 +0800

    update disabled color

--- src/pages/detail/components/receivedExchange/index.tsx
+++ src/pages/detail/components/receivedExchange/index.tsx
@@ -39,7 +39,6 @@ export default function ReceivedExchange(props: ReceivedExchangeProps) {
             key={exchange.id}
             exchange={exchange}
             productClick={handleProductClick}
-            key={exchange.id}
           >
             {isGoodsOwner ? (
               status === ExchangeStatus.APPLIED ? (
